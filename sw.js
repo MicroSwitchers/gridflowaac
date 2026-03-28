77204049d66ed0b31da710d161035ee3f5fd2f0d
MicroSwitchers <141078536+MicroSwitchers@users.noreply.github.com>
2026-03-27 22:13:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * the 'controllerchange' event and reloads automatically so users always
  * run the latest code.
  */
-const CACHE = 'aac-v1';
+const CACHE = 'aac-v3';
 
 const CORE = ['./', './index.html'];
 
